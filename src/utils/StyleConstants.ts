144fefe357e53f1e4cc77815556b4d26c78e06bf
cyrusn <cyrusncy@gmail.com>
2019-03-13 15:48:40 +0800

    Use material color code

--- src/utils/StyleConstants.ts
+++ src/utils/StyleConstants.ts
@@ -1,17 +1,34 @@
+enum COLOR {
+  CYAN_DARK = '#4DD0E1',
+  CYAN_LIGHT = '#00838F',
+  TEAL_DARK = '#26A69A',
+  TEAL_LIGHT = '#00695C',
+  PURPLE_DARK = '#7E57C2',
+  PURPLE_LIGHT = '#4527A0',
+  RED_DARK = '#E57373',
+  RED_LIGHT = '#B71C1C',
+  ORANGE_DARK = '#FFB74D',
+  ORANGE_LIGHT = '#EF6C00',
+  BLUE_DARK = '#42A5F5',
+  BLUE_LIGHT = '#0D47A1',
+  YELLOW_DARK = '#FFEE58',
+  YELLOW_LIGHT = '#F9A825'
+}
+
 export default class StyleConstants {
 
-  public static readonly PROJECT_DARK = 'rgb(186, 234, 244)';
-  public static readonly PROJECT_LIGHT = 'rgb(21, 141, 168)';
-  public static readonly CONTEXT_DARK = 'rgb(190, 244, 186)';
-  public static readonly CONTEXT_LIGHT = 'rgb(91, 124, 88)';
-  public static readonly DATE_DARK = 'rgb(244, 244, 186)';
-  public static readonly DATE_LIGHT = 'rgb(135, 135, 29)';
-  public static readonly PRIORITY_DARK = 'rgb(209, 186, 244)';
-  public static readonly PRIORITY_LIGHT = 'rgb(117, 43, 229)';
-  public static readonly OVERDUE_DARK = 'rgb(229, 115, 115)';
-  public static readonly OVERDUE_LIGHT = 'rgb(229, 115, 115)';
-  public static readonly THRESHOLD_DARK = 'rgb(139, 195, 74)';
-  public static readonly THRESHOLD_LIGHT = 'rgb(139, 195, 74)';
+  public static readonly PROJECT_DARK = COLOR.BLUE_DARK;
+  public static readonly PROJECT_LIGHT = COLOR.BLUE_LIGHT;
+  public static readonly CONTEXT_DARK = COLOR.ORANGE_DARK;
+  public static readonly CONTEXT_LIGHT = COLOR.ORANGE_LIGHT;
+  public static readonly DATE_DARK = COLOR.YELLOW_DARK;
+  public static readonly DATE_LIGHT = COLOR.YELLOW_LIGHT;
+  public static readonly PRIORITY_DARK = COLOR.PURPLE_DARK;
+  public static readonly PRIORITY_LIGHT = COLOR.PURPLE_LIGHT;
+  public static readonly OVERDUE_DARK = COLOR.RED_DARK;
+  public static readonly OVERDUE_LIGHT = COLOR.RED_LIGHT;
+  public static readonly THRESHOLD_DARK = COLOR.TEAL_DARK;
+  public static readonly THRESHOLD_LIGHT = COLOR.TEAL_LIGHT;
 
   public static readonly COMPLETED_CSS = 'font-style: italic; text-decoration: line-through; opacity: 0.5;';
 }
